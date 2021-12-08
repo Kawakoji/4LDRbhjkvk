dabe17825c7c2ce4342d447962e58a2e1c925f9f
adamhamadi <80585866+Kawakoji@users.noreply.github.com>
2021-12-08 18:55:03 +0100

    Add files via upload

---
+++ hehe/app.js
@@ -0,0 +1,7 @@
+menu  const =  document . querySelector ( '#mobile-menu' ) ;
+const  menuLinks  =  document . querySelector ( '.navbar__menu' ) ;
+
+menu . addEventListener ( 'clic' ,  fonction ( )  {
+  menu . classListe . toggle ( 'est-actif' ) ;
+  menuLiens . classListe . bascule ( 'actif' ) ;
+} ) ;
